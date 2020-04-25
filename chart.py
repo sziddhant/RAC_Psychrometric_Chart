5a0531633d7a9e516382ff3e921faee1749f8656
sziddhant <sziddhant@gmail.com>
2020-04-25 11:27:47 +0530

    preliminary plot function added

--- chart.py
+++ chart.py
@@ -1,5 +1,5 @@
 import numpy as np 
-
+import matplotlib.pyplot as plt
 ##Given
 specific_volume= np.arange(start=0.8, stop=0.95, step=0.05)
 relative_humidity= np.arange(start=0.1, stop=1, step=0.1)
@@ -13,9 +13,9 @@ WBT=np.arange(start=5, stop=30, step=5)
 #h= (1.005 + 1.88*W)*t + 2500*W # enthalpy
 enthalpy= []
 
-for i in range(0,len(specific_humidity)-1):
-    for j in range(0,len(DBT)-1 ):
+for i in range(0,len(specific_humidity)):
+    for j in range(0,len(DBT)):
         h=(1.005 + 1.88*specific_humidity[i])*DBT[j] + 2500*specific_humidity[i]
         enthalpy.append(h)
-print(enthalpy)
-        
+    plt.plot(enthalpy,DBT,specific_humidity)
+    plt.show()
