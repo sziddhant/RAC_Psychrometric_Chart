89fb77d4d4d34648c40106057e313a4ac8a1505c
sziddhant <sziddhant@gmail.com>
2020-04-25 10:10:06 +0530

    Initial commit

---
+++ chart.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+##Given
+specific_volume= np.arange(start=0.8, stop=0.95, step=0.05)
+relative_humidity= np.arange(start=0.1, stop=1, step=0.1)
+#DBT= np.arange(start=5, stop=50, step=)
+#specific_humidity =np.arange(start=0, stop=0.03, step=)
+WBT=np.arange(start=5, stop=30, step=5)
+print(WBT[0])
